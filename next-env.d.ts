37e811996eef00f36ea62c58a19e1e9fc835c822
tsbarkai <tsbarkai@gmail.com>
2026-04-13 11:26:59 -0400

    Update TypeScript configuration to target ES2017, enhance strictness, and include incremental builds. Adjust library settings and paths for improved module resolution.

---
+++ next-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file is not checked by tools that validate TypeScript config.
+// See https://nextjs.org/docs/app/api-reference/config/typescript for more information.
